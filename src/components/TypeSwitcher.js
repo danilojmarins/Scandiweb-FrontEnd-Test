23d8283b72833cc50a2aed1adfe0c83d2a7d605e
danilojmarins <danilojmarins@hotmail.com>
2022-05-04 01:52:28 -0300

    specification handle change implemented

--- src/components/TypeSwitcher.js
+++ src/components/TypeSwitcher.js
@@ -5,15 +5,17 @@ class TypeSwitcher extends React.Component {
         super(props);
         this.state = {type: 'Select'};
 
-        this.handleChange = this.handleChange.bind(this);
-        
+        this.handleChange = this.handleChange.bind(this); 
     }
 
     handleChange(e) {
         this.setState({ type: e.target.value });
+        console.log(this.props.Specific);
     }
 
-    render({changeProductData, Specification}) {
+    
+
+    render() {
         if(this.state.type === 'Select') {
             return(
                 <div className='item'>
@@ -41,7 +43,7 @@ class TypeSwitcher extends React.Component {
 
                     <div className='item'>
                         <label className='label'>Size (MB)</label>
-                        <input type='text' id='size' name='Specification' value={Specification} onChange={changeProductData}></input>
+                        <input type='text' id='size' name='dvdSpeci' value={this.props.dvdSpeci} onChange={this.props.changeProduct}></input>
                     </div>
                 </div>
             )
@@ -60,7 +62,7 @@ class TypeSwitcher extends React.Component {
 
                     <div className='item'>
                         <label className='label'>Weight (KG)</label>
-                        <input type='text' id='weight'></input>
+                        <input type='text' id='weight' name='bookSpeci' value={this.props.bookSpeci} onChange={this.props.changeProduct}></input>
                     </div>
                 </div>
             )
@@ -79,17 +81,17 @@ class TypeSwitcher extends React.Component {
 
                     <div className='item'>
                         <label className='label'>Height (CM)</label>
-                        <input type='text' id='height'></input>
+                        <input type='text' id='height' name='furnitSpeciH' value={this.props.furnitSpeciH} onChange={this.props.changeProduct}></input>
                     </div>
         
                     <div className='item'>
                         <label className='label'>Width (CM)</label>
-                        <input type='text' id='width'></input>
+                        <input type='text' id='width' name='furnitSpeciW' value={this.props.furnitSpeciW} onChange={this.props.changeProduct}></input>
                     </div>
         
                     <div className='item'>
                         <label className='label'>Length (CM)</label>
-                        <input type='text' id='length'></input>
+                        <input type='text' id='length' name='furnitSpeciL' value={this.props.furnitSpeciL} onChange={this.props.changeProduct}></input>
                     </div>
                 </div>
             )
--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -8,12 +8,15 @@ import { useNavigate } from 'react-router-dom';
 
 const AddProduct = () => {
 
-    const initialValue = {SKU: '', Name: '', Price: '', Specification: ''};
-    const [product, setProduct] = useState(initialValue);
+    const productInitialValue = {SKU: '', Name: '', Price: '', Specification: ''};
+    const [product, setProduct] = useState(productInitialValue);
+    const specificationInitialValue = {dvdSpeci: '', bookSpeci: '', furnitSpeciH: '', furnitSpeciW: '', furnitSpeciL: ''};
+    const [specification, setSpecification] = useState(specificationInitialValue);
     const [savingProduct, setSavingProduct] = useState(false);
     const [errors, setErrors] = useState({});
 
     const { SKU, Name, Price, Specification } = product;
+    const { dvdSpeci, bookSpeci, furnitSpeciH, furnitSpeciW, furnitSpeciL } = specification;
 
     async function saveProduct(e) {
         e.preventDefault();
@@ -28,8 +31,16 @@ const AddProduct = () => {
 
 
     const changeProductData = e => {
-        setProduct({...product, [e.target.name]: e.target.vaule});
+        setProduct({...product, [e.target.name]: e.target.value});
+        setSpecification({...specification, [e.target.name]: e.target.value})
         setErrors({});
+        console.log(product);
+        console.log(specification);
+    }
+
+
+    const specificationForm = () => {
+        
     }
 
 
@@ -47,7 +58,7 @@ const AddProduct = () => {
             <hr></hr>
 
             <div className='form'>
-                <form id='#product_form' method='post'>
+                <form id='product_form' method='post' >
 
                     <div className='item'>
                         <label className='label'>SKU</label>
@@ -64,7 +75,7 @@ const AddProduct = () => {
                         <input type='text' id='price' name='Price' value={Price} onChange={changeProductData}></input>
                     </div>
 
-                    <TypeSwitcher changeProductData={changeProductData} Specification={product.Specification}/>
+                    <TypeSwitcher changeProduct={changeProductData} dvdSpeci={dvdSpeci} bookSpeci={bookSpeci} furnitSpeciH={furnitSpeciH} furnitSpeciW={furnitSpeciW} furnitSpeciL={furnitSpeciL}/>
 
                 </form>
             </div>
